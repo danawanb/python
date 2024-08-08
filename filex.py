34a2219539c5920a3073421abf237e21ebf4ef95
danawan <danawanbimantoro23@gmail.com>
2024-08-08 13:13:10 +0700

    filex

--- filex.py
+++ filex.py
@@ -5,3 +5,16 @@ contents = path.read_text()
 contents = contents.rstrip()
 print(contents)
 
+
+try:
+
+    paath = Path('test.txt')
+
+    conts = path.read_text()
+    conts = conts.split()
+
+    for cont in conts:
+        print(cont)
+
+except Exception as err:
+    print(err)
